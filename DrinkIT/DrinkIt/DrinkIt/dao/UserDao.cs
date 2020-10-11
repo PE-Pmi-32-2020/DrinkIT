fb4af9b1083f8be8601918729063fa95ba02d314
AndriyPyzh <andriy.pyzh2412@gmail.com>
2020-10-11 19:23:12 +0300

    created project

---
+++ DrinkIT/DrinkIt/DrinkIt/dao/UserDao.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DrinkIt.dao
+{
+    public class UserDao
+    {
+        public static void Main()
+        {
+            string connectionString =
+                @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database2.mdf;Integrated Security=True";
+
+            string commandText = "INSERT INTO MyTable (ID, Name, Address) VALUES (10, 'Bob', '123 Main Street');";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(commandText, conn))
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+    }
+}
