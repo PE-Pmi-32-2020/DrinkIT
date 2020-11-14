68536d55ad25d3fc6bb333437783b50f6c903fce
MartaKostetska <martakostetska1114@gmail.com>
2020-11-14 10:38:15 +0200

    added user registration

--- DrinkIT/DrinkIt/DrinkIt/views/Register.xaml.cs
+++ DrinkIT/DrinkIt/DrinkIt/views/Register.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 using System.Globalization;
+using Npgsql;
+using System.Diagnostics;
 
 namespace WpfApp1
 {
@@ -29,6 +33,8 @@ namespace WpfApp1
             InitializeComponent();
             profile = new Profile();
         }
+
+    
 
         public static bool IsValidEmail(string email)
         {
@@ -57,6 +63,7 @@ namespace WpfApp1
             {
                 return false;
             }
+            
 
             try
             {
@@ -86,13 +93,90 @@ namespace WpfApp1
             string email = EmailBox.Text;
             string password = PasswordBox.Password;
             string confirmpassword = ConfirmPasswordBox.Password;
-
+            
+            
             if (IsValidEmail(email))
             {
                 if (isValidPassword(password) && isValidPassword(confirmpassword))
                 {
                     if (password == confirmpassword)
                     {
+                        try
+                        {
+                            string conStr = "Host=localhost;Username=postgres;Password=1111;Database=postgres";
+                        
+                            NpgsqlConnection Connection = new NpgsqlConnection(conStr);
+                            Connection.Open();
+                            var query = "insert into users (username,password) values(@username,@password)";
+                            var cmd = new NpgsqlCommand(query, Connection);
+                            cmd.Parameters.Add(new NpgsqlParameter("username", email));
+                            cmd.Parameters.Add(new NpgsqlParameter("password", password));
+                            cmd.ExecuteNonQuery();
+                            Connection.Close();
+                        }
+                        catch (Exception exception)
+                        {
+                            System.Windows.MessageBox.Show(exception.Message);
+                            throw;
+                        }
+                       
+                         //string query = "SELECT count(*) fromUser";
+                          // NpgsqlCommand cmd = new NpgsqlCommand( );
+                          // cmd.Connection = Connection;
+                          // cmd.CommandText="insert into users (id,username,password) values(1,'mas@gmail.com','121212n')";
+                          // cmd.ExecuteNonQuery();
+
+                          //var  res =  cmd.ExecuteScalar().ToString();
+                          //System.Windows.MessageBox.Show(res);
+                          
+              
+                         // Connection.Close();
+                          // using (var cmd = new NpgsqlCommand("INSERT INTO users(id,username,password)  VALUES (:u,:p)",Connection))
+                        // {
+                        //     cmd.Parameters.Add(new NpgsqlParameter("u", email));
+                        //     cmd.Parameters.Add(new NpgsqlParameter("p", password));
+                        //     cmd.ExecuteNonQuery();
+                        // }
+                       //  string query = "insert into Users (id,username,password) values(@username,@password)";
+                       //  NpgsqlCommand cmd = new NpgsqlCommand(query, Connection);
+                       //  cmd.Parameters.AddWithValue("username", email);
+                       //  cmd.Parameters.AddWithValue("password", password);
+                       //  
+                       // // cmd.CommandType = CommandType.Text;  
+                       // cmd.ExecuteNonQuery();
+                       // Connection.Close();
+                        
+                       // string query =@"select * from insert_something('asas','asas')"; 
+                       // var cmd = new NpgsqlCommand(query, Connection);
+                       //cmd.CommandType = CommandType.Text;  
+                       // cmd.Parameters.Add(new NpgsqlParameter("username", email));
+                       // cmd.Parameters.Add(new NpgsqlParameter("password", password));
+                             
+                             
+                       //
+                       //NpgsqlCommand cmd = new NpgsqlCommand(@"select * from insert_something(:_val ,:_val2)",
+                       //Connection);
+                       // cmd.Parameters.AddWithValue("_val", "ass@gmail.com");
+                       // cmd.Parameters.AddWithValue("_val2", "as122222");
+                       // cmd.ExecuteNonQuery();
+                       // if ((int) cmd.ExecuteScalar() == 22)
+                       // {
+                       //     System.Windows.MessageBox.Show("ok");
+                       //     
+                       // }
+                       // else
+                       // {
+                       //     System.Windows.MessageBox.Show("not ok");
+                       // }
+                       // var sql2 = "select count(*) from users";
+                       // var cmd2 = Connection.CreateCommand();
+                       // cmd2.CommandText = sql2;
+                       // var count = cmd2.ExecuteScalar().ToString();
+                       // System.Windows.MessageBox.Show(count);
+                       // // 
+                       //cmd.ExecuteNonQuery();
+                       //var count = cmd.ExecuteScalar().ToString();
+                       //System.Windows.MessageBox.Show(count);
                         this.Close();
                         profile.Show();
                     }
@@ -121,5 +205,9 @@ namespace WpfApp1
         {
             Application.Current.Shutdown();
         }
+    }
+
+    internal class MassageBox
+    {
     }
 }
