b6f34233625e69567230534f6cfe806ac68bfbda
Ihor Medvid <ihormed9@gmail.com>
2020-12-15 00:41:01 +0200

    added notification service

---
+++ DrinkIT/DrinkIt/DrinkIt/PL/Notification.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Position;
+
+namespace DrinkIt.PL
+{
+    /// <summary>
+    /// Логика взаимодействия для Notification.xaml
+    /// </summary>
+    public partial class Notification : Window
+    {
+        public Notification()
+        {
+            InitializeComponent();
+            
+        }
+        public  void ShowWindow()
+        {
+            this.Show();
+            _textBlock.Text = "TIME TO DRINK!";
+        }
+
+        Notifier notifier = new Notifier(cfg =>
+        {
+            cfg.PositionProvider = new WindowPositionProvider(
+                parentWindow: Application.Current.MainWindow,
+                corner: Corner.TopRight,
+                offsetX: 10,
+                offsetY: 10);
+
+            cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+                notificationLifetime: TimeSpan.FromSeconds(3),
+                maximumNotificationCount: MaximumNotificationCount.FromCount(5));
+
+            cfg.Dispatcher = Application.Current.Dispatcher;
+        });
+    }
+}
---
+++ DrinkIT/DrinkIt/DrinkIt/bll/NotificationService.cs
@@ -0,0 +1,37 @@
+﻿namespace DrinkIt.BLL
+{
+    using System;
+    using FluentScheduler;
+    using DrinkIt.PL;
+    using System.Windows.Threading;
+
+    public class NotificationService
+    {
+        private Notification _notification;
+        public NotificationService()
+        {
+            JobManager.AddJob(
+                this.DoNotification,
+                notification => notification.ToRunNow().AndEvery(1).Seconds());
+        }
+
+        private void DoNotification()
+        {
+            DoPrimaryWorkOffUIThread();
+            Dispatcher.CurrentDispatcher.Invoke(new Action(this.ShowResultsOnUIThread));
+        }
+
+        private DateTime currentResult;
+
+        private void DoPrimaryWorkOffUIThread()
+        {
+            this.currentResult = DateTime.Now;
+        }
+
+        private void ShowResultsOnUIThread()
+        {
+            _notification = new Notification();
+            _notification.ShowWindow();
+        }
+    }
+}
